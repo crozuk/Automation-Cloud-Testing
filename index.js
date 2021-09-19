32d1e193baec9a42b66089c188e651637e66ab67
play.js <git@gituser.com>
2021-09-19 23:23:22 +0100

    interval json file and object update

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const fs = require('fs')
 var http = require('http');
 
-const data = fs.readFileSync('response.json', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-});
+data = fs.readFileSync('response.json', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //console.log(JSON.parse(data));  
+  });
 
-console.log(JSON.parse(data));
+//Fetch updated JSON (2 days)
+setInterval(function () {
+  
+  //Fetch updated table
+  //require('child_process').fork('fetch.js');
+  
+  data = fs.readFileSync('response.json', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //console.log(JSON.parse(data));  
+  });
+  
+  //console.log("beep")
+  console.log(JSON.parse(data))
+
+}, 172800000)
+
+
+//Fetch updated table (3 days)
+setInterval(function () {
+  
+  //Fetch updated table
+  require('child_process').fork('fetch.js');
+  
+
+}, 259200000)
 
 //create a server object:
 http.createServer(function (req, res) {
